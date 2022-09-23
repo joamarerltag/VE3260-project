089b3d76264a0fc7087c7406c67126d71f530d1f
tageh <tage.hisdal@gmail.com>
2022-09-23 08:14:02 +0000

    Changed to unshare2

--- changeRoot.sh
+++ changeRoot.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+ROTFS=$PWD/unshare-container2
+if [ ! -d $ROTFS ];then
+    mkdir -p $ROTFS/{bin,proc,etc,var/www}
+    cd       $ROTFS/bin/
+    cp       /bin/busybox .
+    for P in $(./busybox --list | grep -v busybox); do ln busybox $P; done;
+    echo "::once:/bin/httpd -p 8080 -h /var/www" >  $ROTFS/etc/inittab
  
-mkdir www/bin
-cd www/bin
+    echo "hallo" >  $ROTFS/var/www/hallo.txt
+ 
+fi
+ 
+sudo PATH=/bin unshare -f -p --mount-proc /usr/sbin/chroot $ROTFS bin/init
 
-cp /bin/busybox .
+#  # Inspisere:
+#  pstree -p UNSHARE_PID
+#  ps -o pid,ppid,uid,tty,cmd PID1 PID2 ...
  
-PRGS=$(./busybox --list)
-for P in $PRGS; do
-	ln -s busybox $P;
-done
+#  # teste: 
+#  curl localhost:8080/hallo.txt
+#  # Starte et shell med (alle) samme naverom som PID
+#  sudo nsenter -t PID -a 
